cd369232b2ad1e64137eb27682e17ea8f0309a95
beodayroi <beodayroi@gmail.com>
2019-10-31 19:39:30 +0700

    update

---
+++ app/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Category extends BaseModel
+{
+    protected $primaryKey = 'id';
+    protected $table = 'categories';
+    protected $fillable = array('name','created_at_ip','updated_at_ip');
+
+}
